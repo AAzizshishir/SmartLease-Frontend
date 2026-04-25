5486fb14f576841bff0c8ad2c4fd420fe24aaae0
shishir <mdabdulaziz3745@gmail.com>
2026-04-25 18:35:51 +0600

    fix next.config file

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/auth/:path*`,
       },
     ];
   },
